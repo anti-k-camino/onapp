2d2d2e24d557f1eedf6d27d157f525c05c0b3c33
Anti-k <romanslyzebniy@gmail.com>
2016-09-22 22:22:34 +0300

    ticket set

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,16 +1,17 @@
 class Ticket < ActiveRecord::Base
-  include Generator
-  validates :name, :email, :subject, :body, :department, :random_id,  presence: true
-  validates :random_id, uniqueness: true
+  include Generator  
   enum department: { dept1: 0,
                      dept2: 1,
                      dept3: 2 }
   enum status: { witing_for_stuff_response: 0,
-                   waiting_for_customer: 1,
-                   on_hold: 2, canceled: 3,
-                   completed: 4 }
-
+                 waiting_for_customer: 1,
+                 on_hold: 2, canceled: 3,
+                 completed: 4 }
+                 
+  validates :name, :email, :subject, :body, :random_id,  presence: true
+  validates :random_id, uniqueness: true
   validates :status, inclusion: { in: statuses.keys }, presence: true
+  validates :department, inclusion: { in: departments.keys }, presence: true
  
  
   def dept
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :subject }
+  it { should validate_presence_of :random_id }
+  it { should validate_presence_of :department }
+  it { should validate_presence_of :status }
+
+  describe '.dept' do
+    let!(:ticket){ create :ticket, department: 2 }
+
+    it 'should return right dept' do
+      expect(ticket.dept).to eq 'Dept3'
+    end
+  end
+
+  describe '.status' do
+    let!(:ticket){ create :ticket, status: 2 }
+
+    it 'should return right dept' do
+      expect(ticket.current_status).to eq 'On hold'
+    end
+  end
 end
