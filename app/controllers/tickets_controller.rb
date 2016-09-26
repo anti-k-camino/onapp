cbec6bfcabf626906177296f1965dfce82b354a5
Anti-k <romanslyzebniy@gmail.com>
2016-09-26 03:39:09 +0300

    stuff_update refactored

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,9 @@
 class TicketsController < ApplicationController
   #before_action :authorize, only:[:stuff_update]  
   before_action :load_ticket, only:[:show, :update, :stuff_update, :edit]
-  after_action :create_mail, only:[:create, :update]  
+  before_action :set_waiting_customer, only:[:stuff_update]
+  after_action :create_mail, only:[:create, :update] 
+
   
   respond_to :js
 
@@ -14,11 +16,7 @@ class TicketsController < ApplicationController
     flash[:notice] = 'Your ticket successfully updated.'  
   end
 
-  def stuff_update   
-    p 'OOOOOOOOOOOOOOOOOOOOO'
-    params[:ticket][:status] = 'waiting_for_customer' if ticket_params[:status] == 'waiting_for_stuff_response'
-    
-    p ticket_params[:status]         
+  def stuff_update                
     @ticket.update(ticket_params) 
     flash[:notice] = 'Ticket has been updated.'       
   end
@@ -29,6 +27,10 @@ class TicketsController < ApplicationController
   protected
   def ticket_params
     params.require(:ticket).permit(:name, :subject, :email, :status, :body, :department, :stuff_id, replies_attributes: [:id, :body])
+  end
+
+  def set_waiting_customer
+    params[:ticket][:status] = 'waiting_for_customer' if ticket_params[:status] == 'waiting_for_stuff_response'
   end
 
   def load_ticket
