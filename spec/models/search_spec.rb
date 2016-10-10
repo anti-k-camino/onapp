f0995f53c270fd9821c49fb3cf4c0760ac591dc5
Anti-k <romanslyzebniy@gmail.com>
2016-10-10 22:41:32 +0300

    search model

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Search do
+  describe '.search_selection' do
+    %w(subject_id random_id).each do |selection|
+      it "call #{selection}" do
+        expect(Ticket).to receive(("by_" + selection).to_sym)
+        Search.search_selection('query', "#{selection}")
+      end
+    end
+  end
+
+  it 'call selection All' do
+    expect(Ticket).to receive(:search).with('query')
+    Search.search_selection('query', 'All')
+  end
+
+  it 'call invalid selection' do
+    expect(ThinkingSphinx).to_not receive(:search).with('query')
+    Search.search_selection('query', 'invalid')
+  end
 end
